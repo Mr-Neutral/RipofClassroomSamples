752a41785456feb69e3593906b74569738d89b71
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-02-04 18:54:08 -0500

    Xaml grid with rowspan / columnspan, spacing, and colors.

--- Chapter3/Chapter3/Chapter3/App.xaml.cs
+++ Chapter3/Chapter3/Chapter3/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Chapter3
         {
             InitializeComponent();
 
-            MainPage = new GridPageCSharp();
+            MainPage = new GridPage();
         }
 
         protected override void OnStart()
