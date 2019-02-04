425d1574c17ed6fdbb5311714c37412d3a8e3df7
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-02-04 18:39:15 -0500

    Grid Xaml version.

--- Chapter3/Chapter3/Chapter3/App.xaml.cs
+++ Chapter3/Chapter3/Chapter3/App.xaml.cs
@@ -11,7 +11,7 @@ namespace Chapter3
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new GridPage();
         }
 
         protected override void OnStart()
