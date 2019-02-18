1e92dcf175314a28e921cdf82b8e5e78ad6275c4
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-02-18 18:12:40 -0500

    Item tapped and selected events

--- Chapter5/Chapter5/Chapter5/MainPage.xaml.cs
+++ Chapter5/Chapter5/Chapter5/MainPage.xaml.cs
@@ -22,5 +22,31 @@ namespace Chapter5
 
             simpleListView.ItemsSource = items;
         }
+
+        private void SimpleListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            DisplayAlert("Item Tapped",
+                        $"{e.Item} was tapped",
+                        "Ok");
+        }
+
+        private void SimpleListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            string selectedItem = "NULL";
+
+            if (e.SelectedItem != null)
+            {
+                selectedItem = e.SelectedItem.ToString();
+            }
+
+            DisplayAlert("Item Selected",
+                        $"{selectedItem} was selected",
+                        "Ok");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            simpleListView.SelectedItem = null;
+        }
     }
 }
