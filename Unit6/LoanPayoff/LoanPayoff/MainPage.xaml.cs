8d1306abb7feaa62d1ffe6004d0ee8ec609de75c
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-03-04 17:56:42 -0500

    Adding calculation

--- Unit6/LoanPayoff/LoanPayoff/MainPage.xaml.cs
+++ Unit6/LoanPayoff/LoanPayoff/MainPage.xaml.cs
@@ -16,7 +16,17 @@ namespace LoanPayoff
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            // Get inputs from the UI store as decimal values
+            double balance = double.Parse(Principle.Text);
+            double interestRate = double.Parse(InterestRate.Text);
+            int months = int.Parse(Months.Text);
+            
+            double convertedRate = interestRate / 100 / 12;
+            double payment = balance *
+                            (convertedRate * Math.Pow((1 + convertedRate), months)) /
+                            (Math.Pow(1 + convertedRate, months) - 1);
 
+            MonthlyPayment.Text = payment.ToString("C");
         }
     }
 }
