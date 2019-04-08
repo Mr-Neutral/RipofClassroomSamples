9256bd6e06b54146756cf87c8816fe3090784364
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-04-08 18:07:39 -0400

    Added custom label with predefined property values

---
+++ Chapter8/Chapter8/Chapter8/CustomLabel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Chapter8
+{
+    public class CustomLabel : Label
+    {
+        public CustomLabel()
+        {
+            FontSize = 40;
+            FontAttributes = FontAttributes.Bold;
+            BackgroundColor = Color.Orange;
+        }
+    }
+}
