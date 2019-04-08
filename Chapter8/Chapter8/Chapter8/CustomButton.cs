22e187b25ad210f07d3a9b663e2a953cb364ba33
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-04-08 18:16:26 -0400

    Added custom button control

---
+++ Chapter8/Chapter8/Chapter8/CustomButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Chapter8
+{
+    public class CustomButton : Button
+    {
+        private int _counter = 0;
+
+        public CustomButton()
+        {
+            this.Clicked += this.OnButtonClicked;
+            this.Text = "0";
+        }
+
+        public int Counter
+        {
+            get => _counter;
+            set
+            {
+                this._counter = value;
+                this.Text = this._counter.ToString();
+            }
+        }
+
+        public void OnButtonClicked(object sender, EventArgs e)
+        {
+            this.Counter++;
+        }
+    }
+}
