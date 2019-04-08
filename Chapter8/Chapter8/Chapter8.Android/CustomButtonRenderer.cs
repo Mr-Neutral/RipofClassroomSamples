b58534879f55f75c004f54c069dacb2fcb10c9d3
Kyle Creekmore <kyle.creekmore@gmail.com>
2019-04-08 18:27:47 -0400

    Adding custom renderer for Android and UWP.

---
+++ Chapter8/Chapter8/Chapter8.Android/CustomButtonRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Chapter8;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton),
+                         typeof(Chapter8.Droid.CustomButtonRenderer))]
+
+namespace Chapter8.Droid
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Chocolate);
+            }
+        }
+    }
+}
---
+++ Chapter8/Chapter8/Chapter8.UWP/CustomButtonRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Chapter8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomButton),
+                          typeof(Chapter8.UWP.CustomButtonRenderer))]
+
+namespace Chapter8.UWP
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var button = Control as Windows.UI.Xaml.Controls.Button;
+                button.Background = new SolidColorBrush(Windows.UI.Colors.Red);
+            }
+        }
+    }
+}
